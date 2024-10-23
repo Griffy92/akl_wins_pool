9cfa0b569cad4d0157eb5ab6163b484aa019c2cf
Dom <126841715+Griffy92@users.noreply.github.com>
2024-10-23 15:45:09 +1300

    Code cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
 import LastSeason from './pages/LastSeason'
+import ThisSeason from './pages/ThisSeason'
+import ThisSeaason from './pages/ThisSeason'
 
 function App() {
 
   return (
-    <>
+    <> 
+      <ThisSeason />
 		  <LastSeason />
     </>
   )
